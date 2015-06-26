c4c5203b944b9386945a83a0c6b4ac8bf128cb46
DigitalResetOfficial <zphaze@outlook.com>
2015-06-26 03:55:38 -0400

    Added Packages

--- elipse.py
+++ elipse.py
@@ -21,6 +21,14 @@ xplugin = open(workspace + "/plugins/" + lplugin)
 plugin = json.load(xplugin)
 pLoad.close()
 print(plugin['name'] + " " + plugin['version'])
+# Load Package
+pacLoad = open("package.json","r")
+packageClass = json.load(pacLoad)
+lpackage = packageClass['package']
+srcPackage = open(workspace + "/packages/" + lpackage)
+package = json.load(srcPackage)
+pacLoad.close()
+print("Package: " + package['name'] + " " + package['version'] + " " + package['arch'])
 # File Name
 fName = raw_input("File Name: ")
 # Create The Project
@@ -36,11 +44,14 @@ while inp == 1:
 		project.close()
 		log.close()
 		xplugin.close()
-	elif li == "plugin.function.description":
+	elif li == "--description":
 		print(plugin['function.description'])
-	elif li == "plugin.function":
+	elif li == "--function":
 		print(plugin['function'])
 		project.write(plugin['function'])
+	elif li == "--pclassload":
+		print(package['addition'])
+		project.write(package['addition'])
 	else:
 		project.write(li + "\n")
 		log.write("Wrote line " + "\"" + li + "\" " + "on project " + fName + " @ " + time.strftime("%I:%M:%S") + "\n")
